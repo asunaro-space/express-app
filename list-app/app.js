9c3f544aa9deba599e82f84dd847d81202ff3988
asunaro-space <treeasunaro577@gmail.com>
2022-09-26 12:17:11 +0900

    syling

--- list-app/app.js
+++ list-app/app.js
@@ -1,2 +1,31 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+app.use(express.static('public'))
+const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'asunarou',
+        password: 'password',
+        database: 'list_app'
+})
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('top.ejs')
+})
+
+app.get('/index', (req, res) => {
+    connection.query((error, results) => {
+        'select * from items',
+
+            console.log(results)
+            res.render('index.ejs')
+        }
+    )
+    res.render('index.ejs')
+})
+
+app.listen(4000);
